e4d6975366a9d1bc1d971f168b96be89d7adf653
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-19 01:01:52 +0300

    task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,52 @@
 #!/usr/bin/python3
-"""0-stats.py"""
-import sys
-import signal
+"""
+A script: Reads standard input line by line and computes metrics
+"""
 
 
-total_file_size = 0
-status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0,
-                      403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
+def parseLogs():
+    """
+    Reads logs from standard input and generates
+    reports
+    """
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
 
-def print_statistics():
-    """Print the accumulated statistics."""
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_code_counts.keys()):
-        if status_code_counts[code] > 0:
-            print(f"{code}: {status_code_counts[code]}")
+def report(fileSize, statusCodes):
+    """
+    Prints generated report to standard output
+    Args:
+        fileSize (int): total log size after every 10 successfully read line
+        statusCodes (dict): dictionary of status codes and counts
+    """
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
 
-def signal_handler(sig, frame):
-    """Handle the keyboard interrupt signal (Ctrl+C)."""
-    print_statistics()
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-
-        if len(parts) < 9 or parts[5] != '"GET' or
-        parts[6] != '/projects/260' or parts[7] != 'HTTP/1.1"':
-            continue
-
-        try:
-            status_code = int(parts[8])
-            file_size = int(parts[9])
-        except (ValueError, IndexError):
-            continue
-        total_file_size += file_size
-        if status_code in status_code_counts:
-            status_code_counts[status_code] += 1
-
-        if line_count % 10 == 0:
-            print_statistics()
-
-except KeyboardInterrupt:
-    print_statistics()
-    sys.exit(0)
+if __name__ == '__main__':
+    parseLogs()
