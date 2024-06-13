9d9a991027bf7d8db4dd985b57274f3aaeb89bc9
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-06-13 14:56:22 +0300

    tasks

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""0-rotate_2d_matrix.py"""
+
+
+def rotate_2d_matrix(matrix):
+    """n x n 2D matrix, rotate it 90 degrees clockwise"""
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(n):
+        matrix[i].reverse()
